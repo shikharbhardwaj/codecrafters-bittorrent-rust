2d05b93e2c714aff117fc46cd033c0c06cfb4579
shikharbhardwaj <shikharbhardwaj68@gmail.com>
2023-10-22 17:00:40 +0530

    Remote unneeded log.

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,6 @@ fn main() {
 
     if command == "decode" {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
-        println!("Logs from your program will appear here!");
-
         // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value);
