c23790efdf44a3bee8cc42bde7526bdb238620bc
shikharbhardwaj <shikharbhardwaj68@gmail.com>
2023-10-22 19:47:36 +0530

    Add use statement.

--- src/bencode.rs
+++ src/bencode.rs
@@ -1,3 +1,5 @@
+use serde_bencode;
+
 pub fn decode_bencoded_value(encoded_value: &str) -> serde_bencode::value::Value {
     let deserialized: serde_bencode::value::Value = serde_bencode::from_str(&encoded_value).unwrap();
 
